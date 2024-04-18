6f2b53ea472cdfd41176978f5a29dd9bfd8d400f
marwantosolve <me.hack1101@gmail.com>
2024-04-18 04:04:27 +0200

    edited some stuff in home

---
+++ phase-2/pages/home/home.js
@@ -0,0 +1,33 @@
+var currentPageUrl = window.location.href;
+
+function highlightActiveLink() {
+  var links = document.querySelectorAll("nav a");
+
+  links.forEach(function (link) {
+    if (link.href === currentPageUrl) {
+      link.classList.add("active");
+    }
+  });
+}
+
+highlightActiveLink();
+
+let menu = document.getElementById("menu-icon");
+let nav = document.getElementById("toogle-nav");
+
+// Toggling navbar in mobile
+menu.addEventListener("click", () => {
+  if (nav.style.left != "0px") {
+    nav.style.display = "flex";
+    setTimeout(() => {
+      nav.style.left = "0px";
+      nav.style.opacity = "1";
+    }, 100);
+  } else {
+    nav.style.left = "100%";
+    nav.style.opacity = "0";
+    setTimeout(() => {
+      nav.style.display = "none";
+    }, 100);
+  }
+});
